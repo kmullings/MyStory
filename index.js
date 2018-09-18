fb5001af9511f08979183d6be0d255dd6c484b9e
Keno Mullings <keno@themullingsgroup.com>
2018-09-18 09:45:57 -0400

    merge fixes.

--- index.js
+++ index.js
@@ -3,11 +3,7 @@ window.onload = function (e) {
 
     if (typeof window.sessionStorage.loggedIn === "undefined" || window.sessionStorage.loggedIn === null || window.sessionStorage.loggedIn === "") {
         alert("You need to be logged in first.");
-<<<<<<< HEAD
-        window.location.href = "/login.3.html";
-=======
         window.location.href = "./login.3.html";
->>>>>>> Made more class updates.
     }
 
     // Get the stories div
@@ -23,39 +19,18 @@ window.onload = function (e) {
 
         // Display each story that belongs the logged in user
         if (story.createdBy == window.sessionStorage.loggedIn) {
-<<<<<<< HEAD
-            const myStoryNode = createStoryNode(story.id);
-=======
             const myStoryNode = createStoryNode(story);
->>>>>>> Made more class updates.
 
             myStoriesNode.appendChild(myStoryNode);
             console.log("It's me!");
         } else {  // Display other stories as well
             const otherStoryNode = createStoryNode(story.id);
-<<<<<<< HEAD
-       
-=======
-
->>>>>>> Made more class updates.
             otherStoriesNode.appendChild(otherStoryNode);
             console.log("It's not me!");
         }
     }
 };
 
-<<<<<<< HEAD
-var createStoryNode = function (storyId) {
-    var storyNode = document.createElement("div");
-    var detailsNode = document.createElement("a");
-    var editNode = document.createElement("a");
-    var deleteNode = document.createElement("a");
-
-    detailsNode.innerHTML = '<a href="story-details.html?id=' + storyId + '"> \
-    <span class="story-title">Fast Track Class</span> \
-</a> |';
-
-=======
 /**
  * 
  * @param {*} story 
@@ -66,21 +41,18 @@ var createStoryNode = function (story) {
     var editNode = document.createElement("a");
     var deleteNode = document.createElement("span");
 
-    detailsNode.innerHTML = '<a href="story-details.html?id=' + story.id + '"> \
-    <span class="story-title">' + story.title + '</span> \
-</a> |';
+    detailsNode.setAttribute("href", "story-details.html?id=story.id");
+    detailsNode.innerHTML = '<span class="story-title">' + story.title + '</span> |';
 
-    editNode.innerHTML = '<a href="upsert-story.html?id="' + story.id + '"> \
-    <span class="edit-button"> \
+    editNode.setAttribute("href", "upsert-details.html?id=story.id");
+    editNode.innerHTML = '<span class="edit-button"> \
         <i>Edit</i> \
-    </span> \
-</a> |';
+    </span> |';
 
     deleteNode.innerHTML = '<span class="delete-button"> \
     <em>Delete</em> \
 </span>';
 
->>>>>>> Made more class updates.
     storyNode.appendChild(detailsNode);
     storyNode.appendChild(editNode);
     storyNode.appendChild(deleteNode);
