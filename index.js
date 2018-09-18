c8502812db11b930a8e0954f61370c3fd17bbc3c
Keno Mullings <keno@themullingsgroup.com>
2018-09-18 10:10:57 -0400

    Fixed error when displaying stories.

--- index.js
+++ index.js
@@ -41,10 +41,10 @@ var createStoryNode = function (story) {
     var editNode = document.createElement("a");
     var deleteNode = document.createElement("span");
 
-    detailsNode.setAttribute("href", "story-details.html?id=story.id");
+    detailsNode.setAttribute("href", "story-details.html?id=" + story.id);
     detailsNode.innerHTML = '<span class="story-title">' + story.title + '</span> |';
 
-    editNode.setAttribute("href", "upsert-details.html?id=story.id");
+    editNode.setAttribute("href", "upsert-details.html?id=" + story.id);
     editNode.innerHTML = '<span class="edit-button"> \
         <i>Edit</i> \
     </span> |';
